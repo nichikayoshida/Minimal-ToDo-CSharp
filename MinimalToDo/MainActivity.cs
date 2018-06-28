51cb5cfd88c4288af670b65819974dd8d09cd204
Nichika Yoshida <n_yoshida@sackle.co.jp>
2018-06-28 13:39:53 +0900

    change icon

--- MinimalToDo/MainActivity.cs
+++ MinimalToDo/MainActivity.cs
@@ -4,7 +4,7 @@ using Android.OS;
 
 namespace MinimalToDo
 {
-    [Activity(Label = "MinimalToDo", MainLauncher = true, Icon = "@mipmap/icon")]
+    [Activity(Label = "MinimalToDo", MainLauncher = true, Icon = "@mipmap/ic_launcher")]
     public class MainActivity : Activity
     {
         int count = 1;
