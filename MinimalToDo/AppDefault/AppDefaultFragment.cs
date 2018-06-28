7724eb4ae8f0f87642ee64c69be57bcfd552b1fc
Nichika Yoshida <n_yoshida@sackle.co.jp>
2018-06-28 15:29:38 +0900

    create AppDefaultFragment

---
+++ MinimalToDo/AppDefault/AppDefaultFragment.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using Android.Support.V4.App;
+
+namespace MinimalToDo.AppDefault
+{
+    public abstract class AppDefaultFragment : Fragment
+    {
+        
+        protected abstract int LayoutResourceId { get; }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            return inflater.Inflate(LayoutResourceId, container, false);
+        }
+    }
+}
