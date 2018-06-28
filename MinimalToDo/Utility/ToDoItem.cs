7c88ec545ae917aaf51b1299732fb28da6298ffc
Nichika Yoshida <n_yoshida@sackle.co.jp>
2018-06-28 17:47:33 +0900

    create ToDoItems.cs

---
+++ MinimalToDo/Utility/ToDoItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Java.IO;
+using Java.Util;
+
+namespace MinimalToDo.Utility
+{
+    public class ToDoItem : Java.Lang.Object ,ISerializable
+    {
+        
+        private readonly string toDoBody;
+        private readonly bool hasReminder;
+        private readonly DateTime toDoDate;
+        private readonly int toDoColor;
+        private readonly UUID toDoIdentifire;
+
+
+        public ToDoItem(string toDoBody, bool hasReminder, DateTime toDoDate)
+        {
+            this.toDoBody = toDoBody;
+            this.hasReminder = hasReminder;
+            this.toDoBody = toDoBody;
+            toDoColor = 1677725;
+            toDoIdentifire = UUID.RandomUUID();
+        }
+    }
+}
